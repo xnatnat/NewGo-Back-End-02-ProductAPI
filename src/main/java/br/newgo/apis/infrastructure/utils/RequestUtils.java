1569ac774a91824b4761697484b8312fe5f58e23
xnatnat <mota.nathalia963@gmail.com>
2023-09-24 13:28:16 -0300

    feat: Adicionado método responsável por ler corpo da requisição

---
+++ src/main/java/br/newgo/apis/infrastructure/utils/RequestUtils.java
@@ -0,0 +1,29 @@
+package br.newgo.apis.infrastructure.utils;
+
+import br.newgo.apis.presentation.dtos.ProdutoDTO;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+/**
+ * Classe utilitária para lidar com operações relacionadas a solicitações HTTP.
+ */
+public class RequestUtils {
+    /**
+     * Lê o corpo da requisição HTTP e o converte para uma string.
+     *
+     * @param req O objeto HttpServletRequest que representa a requisição HTTP.
+     * @return Uma string contendo o corpo da requisição.
+     * @throws RuntimeException Se ocorrer um erro ao ler o corpo da requisição.
+     */
+    static String lerCorpoDaRequisicao(HttpServletRequest req)  {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream()))) {
+            return reader.lines().collect(Collectors.joining());
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao ler o corpo da requisição: " + e.getMessage(), e);
+        }
+    }
+}
